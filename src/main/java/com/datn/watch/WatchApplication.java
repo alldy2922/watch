8b8b4734134855f26de3041aa8e0a6f0d34514ba
tobi <tai.mongtri@gmail.com>
2024-04-09 09:48:33 +0700

    Initial commit

---
+++ src/main/java/com/datn/watch/WatchApplication.java
@@ -0,0 +1,13 @@
+package com.datn.watch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datn/watch/WatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datn.watch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
